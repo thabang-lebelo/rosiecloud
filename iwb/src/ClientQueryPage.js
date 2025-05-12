ef5174da582f25c64569d1c2243b04f50e4607f3
thabang-lebelo <thabangmoseslebelo@gmail.com>
2025-05-12 23:24:42 +0200

    Update ClientQueryPage.js

--- iwb/src/ClientQueryPage.js
+++ iwb/src/ClientQueryPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://rosiecloud.onrender.com';
 
 const ClientQueryPage = () => {
   // Form state
